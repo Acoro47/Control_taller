642948e69119fd74ff906d0c2536c132eb0fe3b1
acoro47 <abelcontry@gmail.com>
2025-08-20 16:49:49 +0200

    añadiendo el modelo de reparaciones y el enum de estados

---
+++ src/main/java/com/taller_control/control_taller/models/Estado.java
@@ -0,0 +1,7 @@
+package com.taller_control.control_taller.models;
+
+public enum Estado {
+	
+	PENDIENTE, EN_REPARACION, ACABADO
+
+}
--- src/main/java/com/taller_control/control_taller/models/Reparacion.java
+++ src/main/java/com/taller_control/control_taller/models/Reparacion.java
@@ -1,8 +1,34 @@
 package com.taller_control.control_taller.models;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Reparacion {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private LocalDateTime fecha_inicio;
+	
+	private LocalDateTime fecha_fin;
+	
+	private String descripcion;
+	
+	private long total_horas;
+	
+	@Enumerated(EnumType.STRING)
+	private Estado estado;
+	
+	@ManyToOne
+	private Vehiculo vehiculo;
 
 }
