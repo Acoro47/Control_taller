a66512f63add90ac08b88d4990a9c396714c0e7e
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-08-21 12:28:20 +0200

    añadiendo los campos de materiales y liquidos

--- src/main/java/com/taller_control/control_taller/models/Liquidos.java
+++ src/main/java/com/taller_control/control_taller/models/Liquidos.java
@@ -10,4 +10,50 @@ public class Liquidos {
 	
 	private Integer precioLitro;
 
+	public Liquidos(Long id, String tipo, Long cantidad, Integer precioLitro) {
+		super();
+		this.id = id;
+		this.tipo = tipo;
+		this.cantidad = cantidad;
+		this.precioLitro = precioLitro;
+	}
+
+	public Liquidos() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Long getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Long cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public Integer getPrecioLitro() {
+		return precioLitro;
+	}
+
+	public void setPrecioLitro(Integer precioLitro) {
+		this.precioLitro = precioLitro;
+	}
+	
+	
+
 }
--- src/main/java/com/taller_control/control_taller/models/Materiales.java
+++ src/main/java/com/taller_control/control_taller/models/Materiales.java
@@ -8,4 +8,41 @@ public class Materiales {
 	
 	private Integer coste;
 
+	public Materiales(Long id, String nombre, Integer coste) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.coste = coste;
+	}
+
+	public Materiales() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getCoste() {
+		return coste;
+	}
+
+	public void setCoste(Integer coste) {
+		this.coste = coste;
+	}
+	
+	
+
 }
